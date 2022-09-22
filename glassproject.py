517882004f7577f280ce768c1344a64621123e5e
Ravi Teja D <rdommeti93@gmail.com>
2022-09-22 08:43:58 +0530

    Updated code

--- glassproject.py
+++ glassproject.py
@@ -46,13 +46,18 @@ vendor_hsil['Source'] = 'HSIL'
 vendor_liverleaf['Source'] = 'Liver Leaf'
 
 #Sorting based on columns in ascending order
-sorted_cp = vendor_cp.sort_index(axis=1)
-sorted_hsil = vendor_hsil.sort_index(axis=1)
-sorted_liverleaf = vendor_liverleaf.sort_index(axis=1)
-
-result_output_file = pds.concat([sorted_hsil,sorted_liverleaf])
+# sorted_cp = vendor_cp.sort_index(axis=1)
+# sorted_hsil = vendor_hsil.sort_index(axis=1)
+# sorted_liverleaf = vendor_liverleaf.sort_index(axis=1)
+result_output_file = pds.concat([vendor_hsil,vendor_liverleaf])
+#Consolidating of HSIL & Liver Leaf from Sample Account 1.xslx
+# result_output_file = pds.concat([sorted_hsil,sorted_liverleaf])
+# Renaming column 'Truck No' to upper case & add space for comparison as one file has upper case and other has lower case
 result_output_file.rename(columns = {'Truck No' : 'TRUCK NO  '},inplace = True)
 #print(list(result_output_file))
+
+# Comparing Output File(Consolidation of HSIL & Liver Leaf) with ACCOUNT_PAYMENT_BANK
+# with TRUCK NO as key. Please note the space in Column name TRUCK NO
 result_final = result_output_file.merge(account_file,how="inner",on = 'TRUCK NO  ')
 #pd.merge(a, b, left_on = 'a_col', right_on = 'b_col', how = 'left')
 #result_final = pds.merge(account_file,result_output_file, left_on = 'TRUCK NO', right_on = 'Truck No', how = 'inner')
@@ -61,10 +66,12 @@ result_final = result_output_file.merge(account_file,how="inner",on = 'TRUCK NO 
 #result_final = pds.concat([result_final,sorted_cp])
 
 #List of column names
-print("\n\n")
+print("\n\n\t\t Output File")
 print(list(result_output_file))
-print("\n\n")
+print("\n\n\t\t Account File")
 print(list(account_file))
+print("\n\n\t\t Final File")
+print(list(result_final))
 
 # print("\n\tSorted Vendor CP")
 # print(sorted_cp.head())
